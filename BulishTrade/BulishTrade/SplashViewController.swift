e0834d816ffc297fe4005431996ed1a38083e9bc
dennisshar <dennis.shar@gmail.com>
2018-05-23 21:33:52 +0300

    loading three dots on splash screen

--- BulishTrade/BulishTrade/SplashViewController.swift
+++ BulishTrade/BulishTrade/SplashViewController.swift
@@ -12,16 +12,21 @@ import UIKit
 class SplashViewController: UIViewController{
     
     
+    @IBOutlet weak var loaginImage: UIImageView!
+    var loadingImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadingImages = createLoadingImageArray(total: 3, imagePrefix: "loading_image")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         
+        animateLoading(imageView: loaginImage,images: loadingImages)
+
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { // change 2 to desired number of seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10) { // change 2 to desired number of seconds
             // Your code with delay
             
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -31,6 +36,27 @@ class SplashViewController: UIViewController{
             
         }
         
+    }
+    
+    func createLoadingImageArray(total: Int, imagePrefix: String) -> [UIImage] {
+        
+        var imageArray: [UIImage] = []
+        
+        for imageCount in 1..<total+1 {
+            let imageName = "\(imagePrefix)_\(imageCount).png"
+            let image = UIImage(named: imageName)!
+            
+            imageArray.append(image)
+        }
+        
+        return imageArray
+    }
+    
+    func animateLoading(imageView: UIImageView,images: [UIImage]){
+        imageView.animationImages = images
+        imageView.animationDuration = 0.7
+        imageView.animationRepeatCount = 10
+        imageView.startAnimating()
     }
     
 }
